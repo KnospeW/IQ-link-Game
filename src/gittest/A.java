a087e317846c5c30acdecf696dbacdfed7ac2afc
u6127601 <duyicong515@163.com>
2016-08-11 10:37:00 +1000

    Alice <u1234567@anu.edu.au>

---
+++ src/gittest/A.java
@@ -0,0 +1,11 @@
+package gittest;
+
+/**
+ * Created by u6127601 on 11/08/16.
+ */
+public class A {
+    @Override
+    public String toString()
+    {return "";}
+
+}
--- src/gittest/Main.java
+++ src/gittest/Main.java
@@ -4,4 +4,6 @@ package gittest;
  * Created by u6127601 on 11/08/16.
  */
 public class Main {
+    public void main()
+    {}
 }
