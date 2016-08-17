b3456ef7305d28b0aaaef3d62b942afa2bc72ff4
joelmcleod <u6061764@anu.edu.au>
2016-08-17 20:28:25 +1000

    Added Piece class, and modified contribution doc

---
+++ src/comp1110/ass2/Piece.java
@@ -0,0 +1,29 @@
+package comp1110.ass2;
+
+/**
+ * Created by joelmcleod on 17/08/2016.
+ */
+
+public class Piece {
+
+    public enum pieceType {
+        LINE, V, WIDEV
+    }
+
+    //direction: six (inc 0) values
+    public Integer findOpening(String direction) {
+
+    }
+
+    //mark piece as array of ball or ring + opening, pull orientation from enum
+    public String definePiece(char pieceName) {
+
+    }
+}
+
+
+
+
+
+
+
