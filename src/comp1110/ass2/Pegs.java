8ee2293f88f52125b904869aa09eacbd0c31de90
u5838800 <u5838800@anu.edu.au>
2016-08-17 21:28:49 +1000

    Tidied up Pegs class.

--- src/comp1110/ass2/Pegs.java
+++ src/comp1110/ass2/Pegs.java
@@ -1,16 +1,17 @@
 package comp1110.ass2;
 
 /**
- * Created by Administrator on 2016/8/17.
+ * Created by Yicong on 2016/8/17.
  */
 
-//  _ _ _ _ _ _ an int array first 3 for ball and last 3 for ring
-  //  _ _ _ the first digits 1/0 indicates whether there exists a ball/ring
-    // the second and third one indicates the direction of openings and connections
+/*  _ _ _ _ _ _ an int array first 3 for ball and last 3 for ring
+      _ _ _ the first digits 1/0 indicates whether there exists a ball/ring
+       the second and third one indicates the direction of openings and connections
 
-    //      1    2
-    //    6        3
-    //      5   4
+            1    2
+          6        3
+            5   4
+*/
 public class Pegs {
     public int [] pegStates= new int[6];
     int ballExit=0;
