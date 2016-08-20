b593b10473a8e18024803b79f4988715ee8b70bc
u5838800 <u5838800@anu.edu.au>
2016-08-20 18:57:56 +1000

    Configured viewer to build a piece object given a placement string.

--- src/comp1110/ass2/gui/Viewer.java
+++ src/comp1110/ass2/gui/Viewer.java
@@ -10,6 +10,7 @@ import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.KeyCode;
 import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
 
@@ -26,8 +27,8 @@ public class Viewer extends Application {
     private static final int SQUARE_SIZE = 100;
     private static final int PIECE_IMAGE_SIZE = 3*SQUARE_SIZE;
     private static final double ROW_HEIGHT = SQUARE_SIZE * 0.8660254; // 60 degrees
-    private static final int VIEWER_WIDTH = 750;
-    private static final int VIEWER_HEIGHT = 500;
+    private static final int VIEWER_WIDTH = 933;
+    private static final int VIEWER_HEIGHT = 700;
 
     private static final String URI_BASE = "assets/";
 
@@ -49,7 +50,36 @@ public class Viewer extends Application {
      */
     void makePlacement(String placement) {
         // FIXME Task 5: implement the simple placement viewer
+        // Will import a location, a piece, and a rotation.
+//        if (root.getChildren().size() > 1) root.getChildren().remove(1);    // removes any pieces present
+        char name = placement.charAt(1);                                    // pulls the name char
+        Character location = placement.charAt(0);                           // pulls the location char
+        Character rotation = placement.charAt(2);                           // pulls the rotation char
 
+        Piece piece = new Piece(name);                                      // creates the piece
+        int rMod = rotation - 'A';                                          // rotation step
+        double sMod = 1;
+        int iSize = 100*(int)sMod;                                          // piece size modifier
+        int row = (location - 'A') % 6;
+        int col = (location - 'A') / 6;
+        double x = 0;
+        if (col % 2 != 0)
+            x = Math.round(Math.cos(rMod*60)) * iSize/2;                    // spacing on odd rows
+        x = x + 25 + row * iSize;
+        int pyt = (iSize * iSize) + ((iSize / 2) * (iSize / 2));            // pythagoras, to save line space
+        double y = 25 + (col * (Math.sqrt(pyt) -25*sMod) );
+
+        piece.setScaleX(sMod);                                              // scale pieces if needed
+        if (rMod > 5) piece.setScaleY(-sMod);                               // flips pieces
+            else piece.setScaleY(sMod);
+        piece.setRotate(60*rMod);                                           // rotate in multiples of 60 deg
+        piece.setX(x);
+        piece.setY(y);
+
+//        System.out.println(placement);                                      // debugging
+//        System.out.println(row + ", " + col);
+//        System.out.println(x + ", " + y);
+        root.getChildren().add(piece);                                      // adds piece to stage
     }
 
     private class Piece extends ImageView {
@@ -57,13 +87,7 @@ public class Viewer extends Application {
             setImage(new Image(Viewer.class.getResource(URL_BASE + id + ".png").toString()));
         }
     }
-//    private void assignImages() {
-//        Image[] imageSet = {A,B,C,D,E,F,G,H,I,J,K,L};
-//        for (int n=0;n<imageSet.length;n++) {
-//            imageSet[n] = new Image(Viewer.class.getResource(URL_BASE + n + ".png").toString());
-//        }
-//        A = new Image(Viewer.class.getResource(URL_BASE + "A.png").toString());
-//    }
+
     /**
      * Create a basic text field for input and a refresh button.
      */
@@ -72,10 +96,13 @@ public class Viewer extends Application {
         textField = new TextField ();
         textField.setPrefWidth(300);
         Button button = new Button("Refresh");
-        button.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent e) {
-                makePlacement(textField.getText());
+        button.setOnAction(e -> {
+            makePlacement(textField.getText());
+            textField.clear();
+        });
+        textField.setOnKeyPressed(k -> {                                    // because Steve should know programmers
+            if (k.getCode() == KeyCode.ENTER) {                             // better than thinking that they ever
+                makePlacement(textField.getText());                         // want to use a mouse.
                 textField.clear();
             }
         });
@@ -94,11 +121,41 @@ public class Viewer extends Application {
 
         root.getChildren().add(controls);
 
+//        makePlacement("BAA");                                               // debugging
+//        makePlacement("HBA");
+//        makePlacement("TCJ");
+//        makePlacement("RDK");
+//        makePlacement("XEB");
+//        makePlacement("EFD");
+//        makePlacement("NGL");
+//        makePlacement("PHE");
+//        makePlacement("DIF");
+//        makePlacement("MJJ");
+//        makePlacement("QKI");
+//        makePlacement("KLJ");
         makeControls();
-//        assignImages();
 
         primaryStage.setScene(scene);
         primaryStage.show();
-        root.getChildren().add(new Piece('A'));
     }
 }
+
+// SCRAPYARD
+
+//import javafx.scene.transform.Rotate;
+//import javafx.scene.transform.Scale;
+//import javafx.scene.transform.Translate;
+
+//        if (rMod > 5) { }
+//        piece.getTransforms().add(new Scale(sMod,sMod));                    // reduces the piece's size a bit
+//        piece.getTransforms().add(new Rotate(60*rMod,x+150,y+150));         // rotates the piece
+//        piece.getTransforms().add(new Translate(x,y));
+//        move(piece,row,col);
+
+//    private void assignImages() {
+//        Image[] imageSet = {A,B,C,D,E,F,G,H,I,J,K,L};
+//        for (int n=0;n<imageSet.length;n++) {
+//            imageSet[n] = new Image(Viewer.class.getResource(URL_BASE + n + ".png").toString());
+//        }
+//        A = new Image(Viewer.class.getResource(URL_BASE + "A.png").toString());
+//    }
