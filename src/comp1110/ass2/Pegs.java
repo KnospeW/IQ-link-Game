ec3bc3a64e5648aaf5eff51e708f6b6797c18201
u5838800 <u5838800@anu.edu.au>
2016-09-15 20:24:20 +1000

    Tidied and reorganised code flow.

--- src/comp1110/ass2/Pegs.java
+++ src/comp1110/ass2/Pegs.java
@@ -13,29 +13,49 @@ package comp1110.ass2;
             5   4
 */
 public class Pegs {
-    public int [] pegStates= new int[6];
-    int ballExit=0;
-    int ringExit=0;
-    int ballCon1,ballCon2,ringOpn1,ringOpn2;
+//    private int[] pegStates;
+    private int ballExit, ringExit, ballCon1, ballCon2, ringOpn1, ringOpn2;
 
-    public Pegs(int[] states)
+    Pegs(int[] states)
     {
-        for (int i=0;i<6;i++) {
-            pegStates[i]=states[i];
-        }
-        this.ballExit=states[0];
-        this.ringExit=states[3];
-        this.ballCon1=states[1];
-        this.ballCon2=states[2];
-        this.ringOpn1=states[4];
-        this.ringOpn2=states[5];
+//        System.arraycopy(states, 0, this.pegStates, 0, 6); // condensed for loop to transfer array values
+        this.ballExit = states[0];
+        this.ringExit = states[3];
+        this.ballCon1 = states[1];
+        this.ballCon2 = states[2];
+        this.ringOpn1 = states[4];
+        this.ringOpn2 = states[5];
 
     }
-    public int[] uptateStates(int [] newstates)
-    {
-        for(int i=0;i>6;i++)
-        {pegStates[i]=newstates[i];}
-        return pegStates;
 
+    public int[] getStateArray(Pegs pegs) {
+        int[] arr = new int[6];
+        arr[0] = this.ballExit;
+        arr[1] = this.ballCon1;
+        arr[2] = this.ballCon2;
+        arr[3] = this.ringExit;
+        arr[4] = this.ringOpn1;
+        arr[5] = this.ringOpn2;
+        return arr;
+    }
+
+    public void updateStates(int[] newStates)
+    {
+//        System.arraycopy(newStates, 0, this.pegStates, 0, 6); // condensed for loop
+//        return pegStates;
+        this.ballExit = newStates[0];
+        this.ballCon1 = newStates[1];
+        this.ballCon2 = newStates[2];
+        this.ringExit = newStates[3];
+        this.ringOpn1 = newStates[4];
+        this.ringOpn2 = newStates[5];
+    }
+
+    public boolean hasBall() {
+        return this.ballExit == 1;
+    }
+
+    public boolean hasRing() {
+        return this.ringExit == 1;
     }
 }
