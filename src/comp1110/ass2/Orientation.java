e368cafb6c1123906507e45dbe1350a441b55c12
u5838800 <u5838800@anu.edu.au>
2016-09-15 19:10:09 +1000

    Added baseline rotate and flip methods.

--- src/comp1110/ass2/Orientation.java
+++ src/comp1110/ass2/Orientation.java
@@ -5,5 +5,57 @@ package comp1110.ass2;
  */
 public enum Orientation {
 
-    OA,OB,OC,OD,OE,OF,OG,OH,OI,OJ,OK,OL
+    OA,OB,OC,OD,OE,OF {
+        @Override
+        public Orientation rotate() {
+            return OA;
+        }
+    },OG {
+        @Override
+        public Orientation flip() {
+            return OA;
+        }
+    },OH {
+        @Override
+        public Orientation flip() {
+            return OB;
+        }
+    },OI {
+        @Override
+        public Orientation flip() {
+            return OC;
+        }
+    },OJ {
+        @Override
+        public Orientation flip() {
+            return OD;
+        }
+    },OK {
+        @Override
+        public Orientation flip() {
+            return OE;
+        }
+    },OL {
+        @Override
+        public Orientation rotate() {
+            return OG;
+        }
+        @Override
+        public Orientation flip() {
+            return OF;
+        }
+    };
+
+//    @Override
+//    public String toString() {
+//        return
+//    }
+
+    public Orientation rotate() {
+        return values()[ordinal() + 1];
+    }
+
+    public Orientation flip() {
+        return values()[ordinal() + 6];
+    }
 }
