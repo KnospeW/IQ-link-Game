7fb58bdf6b3f3cc4a7fce17eb73f7e66ca739e4b
u5838800 <u5838800@anu.edu.au>
2016-09-15 20:05:55 +1000

    Made a start on implementing the board.

--- src/comp1110/ass2/gui/Board.java
+++ src/comp1110/ass2/gui/Board.java
@@ -3,12 +3,20 @@ package comp1110.ass2.gui;
 import comp1110.ass2.LinkGame;
 import comp1110.ass2.Pegs;
 import javafx.application.Application;
+import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
 import javafx.stage.Stage;
 
 public class Board extends Application {
     private static final int BOARD_WIDTH = 933;
     private static final int BOARD_HEIGHT = 700;
+    private static final int SQUARE_SIZE = 100;
+    private static final int PIECE_IMAGE_SIZE = 3*SQUARE_SIZE;
+    private static final double ROW_HEIGHT = SQUARE_SIZE * 0.8660254; // 60 degrees
+
+    private Pegs[] board = new Pegs[24];
 
     // FIXME Task 8: Implement a basic playable Link Game in JavaFX that only allows pieces to be placed in valid places
 
@@ -18,13 +26,33 @@ public class Board extends Application {
 
     // FIXME Task 12: Generate interesting starting placements
 
-    Pegs pegs;
+    class draggablePiece extends ImageView {
 
-    //create our Borad,have 12 grey circle backgrounds
+    }
+
+
+    //create our Board, have 12 grey circle backgrounds
     //it is the initial container, the center will be the solution area
     public void createBoard()
     {
+        int[] blank = {0,0,0,0,0,0};
+        for (Pegs peg : board) {
+            peg.updateStates(blank);
+        }
 
+        for (int i = 0; i < 24; i++) {
+            double x = 0, y;
+            int col = i / 6;
+            int row = i % 6;
+            if (col % 2 != 0)
+                x =  SQUARE_SIZE / 2;
+            x = x + 25 + row * SQUARE_SIZE+150;
+            y = 25 + (ROW_HEIGHT * col)+150;
+
+
+            Circle a = new Circle(x,y,30, Color.GRAY);
+//            root.getChildren().add(a);
+        }
     }
     // create each piece
     public void drawPiece()
@@ -32,12 +60,12 @@ public class Board extends Application {
 
     }
     //  change the look of the selected piece and rotate it when a MouseEvent is captured
-    public void rotatePiece(MouseEvent e )
+    public void rotatePiece(MouseEvent e)
     {
 
     }
     // flip the selected piece when a MouseEvent is captured
-    public void flipPiece(MouseEvent e ) {
+    public void flipPiece(MouseEvent e) {
 
     }
     //change the board when the user attemped to move the piece
