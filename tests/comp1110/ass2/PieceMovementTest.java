ab9206bcc928c50545f4324aba33afc110025ea2
u5838800 <u5838800@anu.edu.au>
2016-09-15 19:09:07 +1000

    Added movement tests.

---
+++ tests/comp1110/ass2/PieceMovementTest.java
@@ -0,0 +1,51 @@
+package comp1110.ass2;
+
+import org.junit.Test;
+
+import java.util.Random;
+
+import static comp1110.ass2.Orientation.*;
+import static org.junit.Assert.*;
+
+/**
+ * Created by azphr on 15/09/16.
+ */
+public class PieceMovementTest {
+
+    @Test
+    public void testRotateIndex() {
+        Piece piece = Piece.A;
+        Orientation[] expected = {OB,OC,OD,OE,OF,OA};
+        Orientation[] output = new Orientation[6];
+        for (int i = 1; i < output.length + 1; i++) {
+            piece.rotatePiece(1);
+            output[i-1] = piece.getOrientation();
+        }
+        boolean equal = true;
+        for (int i = 0; i < output.length; i++) {
+            equal = equal && (output[i].equals(expected[i]));
+        }
+        String out = "", exp = "";
+        for (Orientation o : output) out = out + o.toString() + " ";
+        for (Orientation o : expected) exp = exp + o.toString() + " ";
+        assertTrue("Simple rotation failed: expected " + exp + "and got " +out, equal);
+    }
+
+    @Test
+    public void testRotateAbs() {
+        Piece piece = Piece.B;
+        Random r = new Random();
+        Orientation o = Orientation.values()[r.nextInt(12)];
+        piece.rotatePiece(o);
+        assertTrue("Absolute rotation failed: expected + " +o.toString()+ " and got " + piece.getOrientation(), piece.getOrientation()==o);
+    }
+
+    @Test
+    public void testFlip() {
+        Piece piece = Piece.C;
+        piece.flipPiece();
+        Orientation exp = piece.getOrientation().flip();
+        assertTrue("Piece flip failed: expected + " +exp.toString()+ " and got " + piece.getOrientation(), piece.getOrientation().flip()==exp);
+    }
+
+}
