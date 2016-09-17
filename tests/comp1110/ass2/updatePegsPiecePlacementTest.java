5c6b82c4bf68a2ff24542321586b34e525fa338c
Yicong <duyicong515@163.com>
2016-09-17 23:31:41 +1000

    change for the tests again as the reference direction for pegs changes to make it the same with the reference direction for pieces

--- tests/comp1110/ass2/updatePegsPiecePlacementTest.java
+++ tests/comp1110/ass2/updatePegsPiecePlacementTest.java
@@ -16,8 +16,8 @@ public class updatePegsPiecePlacementTest {
       String PiecePlacement="HBA";
       int states[]=LinkGame.updatePegsPiecePlacement(PiecePlacement);
         assertTrue("there should be a ring with no opening in the origin but got "+Arrays.toString(states),states[0]==0&&states[3]==1&&states[4]==0&&states[5]==0);
-        assertTrue("there should be a ball with connection3 in the br1 but got "+ Arrays.toString(states),states[6]==1&&states[7]==3&&states[8]==0&&states[9]==0);
-        assertTrue("there should be a ring with opening2 in the br2 but got "+Arrays.toString(states),states[12]==0&&states[15]==1&&states[16]==2&&states[17]==0);
+        assertTrue("there should be a ball with connection3 in the br1 but got "+ Arrays.toString(states),states[6]==1&&states[7]==4&&states[8]==0&&states[9]==0);
+        assertTrue("there should be a ring with opening2 in the br2 but got "+Arrays.toString(states),states[12]==0&&states[15]==1&&states[16]==3&&states[17]==0);
     }
     @Test
     public void testrotating()
@@ -25,13 +25,13 @@ public class updatePegsPiecePlacementTest {
         String PiecePlacement0="WEB";
         int states[]=LinkGame.updatePegsPiecePlacement(PiecePlacement0);
         assertTrue("there should be a ring with no opening in the origin but got "+Arrays.toString(states),states[0]==0&&states[3]==1&&states[4]==0&&states[5]==0);
-        assertTrue("there should be a ball with connection4 in the br1 but got "+Arrays.toString(states),states[6]==1&&states[7]==4&&states[8]==0&&states[9]==0);
-        assertTrue("there should be a ring with opening1 in the br2 but got "+Arrays.toString(states),states[12]==0&&states[15]==1&&states[16]==1&&states[17]==0);
+        assertTrue("there should be a ball with connection4 in the br1 but got "+Arrays.toString(states),states[6]==1&&states[7]==5&&states[8]==0&&states[9]==0);
+        assertTrue("there should be a ring with opening1 in the br2 but got "+Arrays.toString(states),states[12]==0&&states[15]==1&&states[16]==2&&states[17]==0);
         String PiecePlacement1="MID";
         int states1[]=LinkGame.updatePegsPiecePlacement(PiecePlacement1);
-        assertTrue("there should be a ball with connection34 in the origin but got "+Arrays.toString(states),states1[0]==1&&states1[1]==3&&states1[2]==4&&states1[3]==0);
-        assertTrue("there should be a ball with connection6 in the br1 but got "+Arrays.toString(states),states1[6]==1&&states1[7]==6&&states1[8]==0&&states1[9]==0);
-        assertTrue("there should be a ring with opening4 in the br2 but got "+Arrays.toString(states),states1[12]==0&&states1[15]==1&&states1[16]==4&&states1[17]==0);
+        assertTrue("there should be a ball with connection34 in the origin but got "+Arrays.toString(states),states1[0]==1&&states1[1]==4&&states1[2]==5&&states1[3]==0);
+        assertTrue("there should be a ball with connection6 in the br1 but got "+Arrays.toString(states),states1[6]==1&&states1[7]==1&&states1[8]==0&&states1[9]==0);
+        assertTrue("there should be a ring with opening4 in the br2 but got "+Arrays.toString(states),states1[12]==0&&states1[15]==1&&states1[16]==5&&states1[17]==0);
     }
     @Test
     public void testflip()
